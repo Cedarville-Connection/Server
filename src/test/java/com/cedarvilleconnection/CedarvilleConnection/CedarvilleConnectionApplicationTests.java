2a25c9fc3826bac6be63d9f6490f9ff6d3c68846
Nathan O'Neel <nathanconeel@gmail.com>
2020-04-02 12:31:50 -0400

    Skeleton Maven + NPM
    Skeleton with Maven, NPM, Tailwind Css, JQuery, JDBC working

---
+++ src/test/java/com/cedarvilleconnection/CedarvilleConnection/CedarvilleConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedarvilleconnection.CedarvilleConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CedarvilleConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
