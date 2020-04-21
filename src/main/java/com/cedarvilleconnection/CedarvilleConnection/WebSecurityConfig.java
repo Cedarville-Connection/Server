d7229b9e3329a15578d0bdb76afb7e6cc414a0de
RPiAwesomeness <keyboardmailtesting@gmail.com>
2020-04-21 14:34:18 -0400

    Registration form looks nicer now?

--- src/main/java/com/cedarvilleconnection/CedarvilleConnection/WebSecurityConfig.java
+++ src/main/java/com/cedarvilleconnection/CedarvilleConnection/WebSecurityConfig.java
@@ -1,21 +1,51 @@
 package com.cedarvilleconnection.CedarvilleConnection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager() throws Exception {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+        jdbcUserDetailsManager.setDataSource(dataSource);
+        return jdbcUserDetailsManager;
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select email, password from users where email=?")
+            .authoritiesByUsernameQuery(
+                "select u.email, u.authority " +
+                "from user_authorities a, users u " +
+                "where u.email = ? " +
+                "and u.id = a.user_id"
+            );
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/css/**").permitAll()
-                .and()
-            .authorizeRequests()
                 .antMatchers("/").authenticated()
+                .antMatchers("/admin").hasAnyRole("admin")
+                .antMatchers("/css/**").permitAll()
                 .and()
             .formLogin()
                 .loginPage("/login")
@@ -25,5 +55,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .httpBasic();
+
+        // Handle CSRF security
+        http.csrf().disable();
     }
 }
--- src/main/java/com/cedarvilleconnection/CedarvilleConnection/controller/RootController.java
+++ src/main/java/com/cedarvilleconnection/CedarvilleConnection/controller/RootController.java
@@ -1,13 +1,25 @@
 package com.cedarvilleconnection.CedarvilleConnection.controller;
 
-import org.springframework.ui.Model;
+import com.cedarvilleconnection.CedarvilleConnection.model.UserRegistration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/")
-public class RootController extends WebMvcConfigurerAdapter {
+public class RootController {
+
+    @Autowired
+    JdbcUserDetailsManager jdbcUserDetailsManager;
+
     @RequestMapping(method = RequestMethod.GET)
     ModelAndView index() {
         ModelAndView mav = new ModelAndView("index");
@@ -15,14 +27,16 @@ public class RootController extends WebMvcConfigurerAdapter {
         return mav;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
-        if (error != null)
-            model.addAttribute("errorMsg", "Your username and password are invalid.");
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView processRegister(
+        @ModelAttribute("user") UserRegistration userRegistrationObject) {
 
-        if (logout != null)
-            model.addAttribute("msg", "You have been logged out successfully.");
+        // authorities to be granted
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("admin"));
 
-        return "login";
+        User user = new User(userRegistrationObject.getEmail(), userRegistrationObject.getPassword(), authorities);
+        jdbcUserDetailsManager.createUser(user);
+        return new ModelAndView("redirect:/welcome");
     }
 }
