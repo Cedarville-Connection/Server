52806fea6e7d413406308b466dfe3b447decdef7
tlow14 <tomlowry14@yahoo.com>
2022-02-25 15:07:10 -0700

    Added deployment files

---
+++ target/classes/static/js/main.js
@@ -0,0 +1,44 @@
+$(function() {
+
+	
+	//$("#post").submit(function(e) {
+	//	e.preventDefault();
+	//	
+	//	var content = $("#post").find("textarea").eq(0).val();
+	//	$.post( "/post", {"postContents": content}, function( data ) {
+    //		location.reload();
+	//	});
+	//});
+
+
+	$('.commentButton').click(function() {
+        $(this).parent().parent().find(".commentBox").toggleClass('open');
+    });
+    
+    $('.likeButton').click(function() {
+    	var postid = $(this).attr("data-postId");
+    	var that = this;
+    	var $amount = $(that).find(".amountOfLikes");
+    	$.post( "/like", {"postId": postid}, function( data ) {
+    		if(data == "ADDED") {
+    			$(that).find(".fa").removeClass("fa-thumbs-o-up").addClass("fa-thumbs-up");
+    			$amount.text(parseInt($amount.text()) + 1);
+    		} else {
+    			$(that).find(".fa").removeClass("fa-thumbs-up").addClass("fa-thumbs-o-up");
+    			$amount.text(parseInt($amount.text()) - 1);
+    		}
+		});
+    });
+    
+    $('.shareButton').click(function() {
+    	var postid = $(this).attr("data-postId");
+    	var that = this;
+    	$.post( "/share", {"postId": postid}, function( data ) {
+    		if(data == "OK") {
+    			$(that).find(".fa").removeClass("fa-share-square-o").addClass("fa-share-square");
+    		}
+		});
+    });
+
+
+});
